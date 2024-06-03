957a0a7921c1ed15603cfbdb3117703dd8931568
Rashid Saleem <mrashidcit@gmail.com>
2024-06-03 09:58:49 +0500

    Tier Insert , Search and delete operations have been completed

--- Section27_Trie/Section27_Trie/src/Main.java
+++ Section27_Trie/Section27_Trie/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Trie newTrie = new Trie();
+        newTrie.insert("API");
+        newTrie.insert("APIS");
+        newTrie.search("APIS");
+        newTrie.delete("API");
+        newTrie.search("API");
     }
 }
