fe061ddef3403a04ae2c7f6df9e4681c6c048ca0
Rashid Saleem <mrashidcit@gmail.com>
2024-07-09 09:31:00 +0500

    Searching Algorighm section completed

---
+++ Section30_Searching_Algorithms/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Linear Search
+//        int[] arr = {1, 3, 2, 10, 23, 11};
+//        Searching.linearSearch(arr, 10);
+
+        // Binary Search
+        int[] arr = {8, 9, 12, 15, 17, 19, 20, 21, 28};
+//        Searching.binarySearch(arr, 15);
+        Searching.binarySearch(arr, 30);
+
+    }
+}
